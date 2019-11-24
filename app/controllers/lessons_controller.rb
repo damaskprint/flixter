eb5a3bf92f7326979faaf1392e0955f5a7e16b0e
Philip Cox <philcogm@gmail.com>
2019-11-24 20:13:54 +0000

    defined permissions to view lessons

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,4 +1,6 @@
 class LessonsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :require_authorized_for_current_lesson
 
   def show
   end
@@ -7,5 +9,16 @@ class LessonsController < ApplicationController
   def current_lesson
     @current_lesson ||= Lesson.find(params[:id])
   end
+
+  private
+
+  def require_authorized_for_current_lesson
+   unless current_user.enrolled_in?(current_lesson.section.course)
+      redirect_to course_path(current_lesson.section.course), alert: 'Must Enroll in Course!'
+   end
+  end
+
+
+  
 
 end
